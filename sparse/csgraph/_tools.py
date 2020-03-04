6e823a0ff7b7954217a0916b176d656050780f2b
saturnman <saturnman@126.com>
2020-03-04 15:09:35 +0800

    add py files

---
+++ sparse/csgraph/_tools.py
@@ -0,0 +1,3 @@
+import sys
+#print dir(_tools._tools)
+reconstruct_path = sys.modules['_tools'].reconstruct_path
---
+++ version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.16.2'
+version = '0.16.2'
+full_version = '0.16.2.dev0+9c67cdd'
+git_revision = '9c67cdd3ce3ff99c286cc391fa7e562e11b9fe6b'
+release = False
+
+if not release:
+    version = full_version
